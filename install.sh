07998e4749b01d14436c3ce9c5f6d97c364ec47f
Dominic Orchard <dom.orchard@gmail.com>
2026-07-16 14:58:38 +0100

    update conda script to force python3.11

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Create a new environment for the project, given as the first argument.
 if [ $# -gt 0 ]
 then
-    conda create -n $1
+    conda create -n $1 python=3.11
     source activate $1
 fi
 
@@ -12,7 +12,7 @@ fi
 conda install -c conda-forge pyresample netCDF4 basemap jsonschema frozendict
 
 # Install project packages.
-pip install -e .
+python -m pip install -e .
 
 # Write environment variables that are used by the project.
 export PYTHONHASHSEED=0
@@ -22,3 +22,8 @@ export ARRHENIUS_MAIN_PATH=`pwd`
 wget -nc -P data/models https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Gridded/Land_and_Ocean_LatLong1.nc
 wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/air.mon.mean.nc
 wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/rhum.mon.mean.nc
+
+if [ $# -gt 0 ]
+then
+  echo "Now run: conda activate $1"
+fi
