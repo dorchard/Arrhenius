dc0459d0392f590afb6075b68d78ef54292c890a
Steve Easterbrook <sme@BA3259D1.Silver.L7QNWW2T41>
2026-07-16 06:48:46 -0400

    Added comments to briefly describe the three datasets

--- install.sh
+++ install.sh
@@ -19,6 +19,9 @@ export PYTHONHASHSEED=0
 export ARRHENIUS_MAIN_PATH=`pwd`
 
 # Download data files from remote sources
+#Berkeley Earth monthly global surface air temperatures from 1948 to 2026, on a 2.5 degree grid
 wget -nc -P data/models https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Gridded/Land_and_Ocean_LatLong1.nc
+#NCEP/NCAR Reanalysis data with monthly global surface air temperatures from 1948 to 2026, on a 2.5 degree grid
 wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/air.mon.mean.nc
+#NCEP/NCAR Reanalysis data with monthly global monthly mean relative humidity from 1948 to 2026, on a 2.5 degree grid
 wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/rhum.mon.mean.nc
