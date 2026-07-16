e588977b6678ebfa6cc9140b7c66ba6dda182611
Dominic Orchard <dom.orchard@gmail.com>
2026-07-16 15:24:30 +0100

    handle errors from wget

--- install.sh
+++ install.sh
@@ -21,8 +21,19 @@ export PYTHONHASHSEED=0
 export ARRHENIUS_MAIN_PATH=`pwd`
 
 # Download data files from remote sources
-wget -nc -P data/models https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Gridded/Land_and_Ocean_LatLong1.nc
-wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/air.mon.mean.nc
-wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/rhum.mon.mean.nc
+download() {
+    local url=$1
+    local dest="data/models/$(basename "$url")"
+    echo "Downloading $url..."
+    if ! wget -nc -P data/models "$url"; then
+        echo "ERROR: Failed to download $url" >&2
+        exit 1
+    fi
+    echo "OK: $dest"
+}
+
+download https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Gridded/Land_and_Ocean_LatLong1.nc
+download ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/air.mon.mean.nc
+download ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/rhum.mon.mean.nc
 
 echo "Now run: conda activate $1"
