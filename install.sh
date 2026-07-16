ad7c11981e0d32742497a83a8711507a01196bc1
Steve Easterbrook <sme@BA3259D1.Silver.L7QNWW2T41>
2026-07-16 06:36:01 -0400

    fixed the link to the Berkeley global land and ocean temperature data (monthly, one degree grid).

--- install.sh
+++ install.sh
@@ -19,6 +19,6 @@ export PYTHONHASHSEED=0
 export ARRHENIUS_MAIN_PATH=`pwd`
 
 # Download data files from remote sources
-wget -nc -P data/models http://berkeleyearth.lbl.gov/auto/Global/Gridded/Land_and_Ocean_LatLong1.nc
+wget -nc -P data/models https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Gridded/Land_and_Ocean_LatLong1.nc
 wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/air.mon.mean.nc
 wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/rhum.mon.mean.nc
